0b76d7da2402f510ea6fe893c1e7ce9ed5a2dc5f
Melchior-cmd <lucas.horda18@gmail.com>
2019-08-27 19:18:35 -0300

    versão 0.1

---
+++ tindev/src/routes.js
@@ -0,0 +1,11 @@
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default createAppContainer(
+    createStackNavigator({
+        Login,
+        Main,
+    })
+);
